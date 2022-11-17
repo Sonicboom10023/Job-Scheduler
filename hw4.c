e6a7031773d5230b5234e0cfab444a3840845525
Sonicboom10023 <101279209+Sonicboom10023@users.noreply.github.com>
2022-11-17 13:00:58 -0600

    Added Job struct and function to create job type
    Also cleaned up some other functions and worked on them more, such as process_input.

--- hw4.c
+++ hw4.c
@@ -15,11 +15,53 @@
 #define JOBSMAX 1000
 #define JOBQMAX 1000
 
+
+typedef struct job{
+    int jobid;     // job id
+    pthread_t tid; //thread id
+    char *command; //command for job to run
+    char *stat;    // job status
+    int exitstat; //job exit status
+    char *start;  // job start time
+    char *stop;   // job stop time
+    char jobout[10]; //filename for stdout redirect
+    char joberr[10]; //filename for err redirect
+} job;
+
+
 int P; //job concurrency based on initial input
 int curworking; //# of currently running jobs
 job Jobs[JOBSMAX]; //global array of inputted jobs
 queue *JobQ; //queue of pointers to jobs
 
+
+void list_jobs(job *jobs, int n, char *mode){
+    int i;
+    if (jobs != NULL && n != 0){
+        if (strcmp(mode, "showjobs") == 0 ){
+            for (i = 0; i < n; i++){
+                printf("Job ID: %d\nCommand: %s\nStatus: %s\n\n", jobs[i].jobid, jobs[i].command, jobs[i].stat);
+            }
+        }
+    }
+}
+
+//create job and fill values
+job create_job(char *command, int jobid){
+    job j;
+    j.jobid = jobid;
+    //initialize temporary value and null terminate it
+    char *temp = malloc(sizeof(char)*strlen(command));
+    temp[strlen(temp)+1] = (char*)strcat(temp, command);
+    j.command = temp;
+    j.stat = "waiting";
+    j.exitstat = -1;
+    j.start = NULL;
+    j.stop = NULL;
+    sprintf(j.jobout,"%d.out", j.jobid);
+    sprintf(j.joberr,"%d.out", j.jobid);
+    return j;
+}
 
 
 
@@ -28,18 +70,23 @@ void *run_job(void *arg){
     char **jobargs; //job arguments, to be parsed
     pid_t pid;
 
-    jobp = (job*)arg;
-
+    jobp = (job *)arg;
+    ++curworking;
+    jobp->stat = "working";
+    time_t tim1 = time(NULL);
+    jobp->start = ctime(&tim1);
 }
 
+//takes input from std and inserts new jobs, while also running commands
 void process_input(){
     int i; //keep track of job #
     char line[LINEMAX]; //reading buffer
     char *key; //first input
     char *command; //command input
+    size_t *buffer = malloc(sizeof(char)*LINEMAX);
 
     i=0;
-    while (getline(line, LINEMAX,stdin)!= -1){  //check input from user
+    while (getline(line, buffer,stdin)!= -1){  //check input from user
         if ((key = strtok(line," \n\t\r"))!= NULL){ //assign first seen value to key, as it will most likely be a
                                                     
             if (strcmp(key, "submit")==0){
@@ -50,10 +97,15 @@ void process_input(){
                     printf("Job queue full, please wait for other jobs to finish.");
                 }
                 else{
+                    command = strtok(line, "\n");
+                    Jobs[i] = create_job(command, i);
+                    queue_insert(JobQ, Jobs + i);
+                    printf("Job %d added to job queue\n", i++);
                 }
             }
         }
     }
+    kill(0,SIGINT);
 }
 
 int main(int argc, char **argv) {
@@ -63,5 +115,5 @@ int main(int argc, char **argv) {
         printf("Usage: %s concurrency wrong\n", argv[0]);
         exit(-1);
     }
-    struct queue_init(queueP, 5);
+    struct queue_init(5);
 }
